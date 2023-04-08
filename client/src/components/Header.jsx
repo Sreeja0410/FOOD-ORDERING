897af96496c6a411b73fb60836a6a78f6a3aa0a0
sreeja <gaddameedisreeja74@gmail.com>
2023-04-08 11:13:15 +0530

    changed UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,7 @@ export const Header = ({ cartCount }) => {
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                 <div className="logo-wrapper pl-4 flex items-center">
                     <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
-                        <img src={foody} alt="logo" className="w-40 h-40 object-cover"/>
+                        <img src={foody} alt="logo" className="padding-10 w-24 h-24 object-cover"/>
                     </Link>
                 </div>
                 <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
